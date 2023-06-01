ef5c82a094e0cf76a3ee89f9abb64e06b89d59eb
ryuji <yrejgnl11@163.com>
2023-06-01 14:28:35 +0800

    update

---
+++ JS/array-method.js
@@ -0,0 +1,27 @@
+/**
+ * JS数组方法
+ */
+
+// push 返回数组长度
+// pop 返回去掉的元素
+// unshift 返回数组长度
+// shift 返回去掉的元素
+// slice 返回截断的数组
+// splice
+// forEach
+// map
+// reduce
+// some
+// every
+// concat 返回合并后的新数组
+// filter
+// fill
+// join
+// sort
+// indexOf
+// includes
+// lastIndexOf
+// toString
+// reverse
+// reduceRight
+
---
+++ Vue/lifecycle.js
@@ -0,0 +1,21 @@
+/**
+ * Vue组件生命周期
+ */
+
+// beforeCreate
+// created
+// beforeMount
+// mounted
+// updated
+// beforeUpdate
+// destroyed (unmounted)
+// beforeDestroy （beforeUnmount）
+// activated
+// deactivated
+
+/**
+ * Vue3 Composition API 生命周期
+ */
+
+// setup
+// onMounted(() => {})
