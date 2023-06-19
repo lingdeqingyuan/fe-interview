d9b8eb4aa84a533b3ec1aee98e80ec3278e753ca
ryuji <yrejgnl11@163.com>
2023-06-19 10:00:48 +0800

    feat: select sort

--- Algorithm/sort/select-sort.js
+++ Algorithm/sort/select-sort.js
@@ -1,7 +1,22 @@
 const { beSortedArr } = require('./test-case');
 
 const selectSort = (beSortedArr) => {
+    const len = beSortedArr.length;
+    let minIndex;
+    for (let i = 0; i < len; i++) {
+        minIndex = i;
+        for (let j = i; j < len; j++) {
+            if (beSortedArr[j] < beSortedArr[minIndex]) {
+                minIndex = j;
+            }
+        }
 
+        if (minIndex !== i) {
+            [beSortedArr[minIndex], beSortedArr[i]] = [beSortedArr[i], beSortedArr[minIndex]];
+        }
+    }
+
+    return beSortedArr
 }
 
 const result = selectSort(beSortedArr);
